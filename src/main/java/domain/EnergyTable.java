2eb019530c43c776aafed0837699193735b082e5
ric-rai <48387692+Ric-Rai@users.noreply.github.com>
2020-03-26 18:24:51 +0200

    Update to extend ArrayTable Add javadoc to diffFunc()

--- src/main/java/domain/EnergyTable.java
+++ src/main/java/domain/EnergyTable.java
@@ -1,48 +1,50 @@
 package domain;
 
+import data_structures.ArrayTable;
 import domain.enums.Color;
 import domain.enums.Direction;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
 import java.util.function.BiFunction;
 
 import static domain.enums.Color.*;
 import static domain.enums.Direction.*;
 
-public class EnergyTable {
+public class EnergyTable extends ArrayTable<Integer> {
     private PixelTable pixelTable;
-    private ArrayList<ArrayList<Integer>> dualGradientEnergies;
+    private boolean dualGradientComputed;
 
-    public EnergyTable(Image image) {
-        pixelTable = new PixelTable((BufferedImage) image);
+    public EnergyTable(BufferedImage image) {
+        super(Integer.class, image.getWidth(), image.getHeight());
+        pixelTable = new PixelTable(image);
     }
 
     private void computeDualGradientEnergyTable() {
         BiFunction<Integer, Integer, Integer>
                 R_x = diffFunc(R, X), G_x = diffFunc(G, X), B_x = diffFunc(B, X),
                 R_y = diffFunc(R, Y), G_y = diffFunc(G, Y), B_y = diffFunc(B, Y);
-        dualGradientEnergies = new ArrayList<>(pixelTable.getWidth());
-        for (int row = 0; row < pixelTable.getHeight(); row++) {
-            ArrayList<Integer> rowList= new ArrayList<>(pixelTable.getWidth());
-            dualGradientEnergies.add(rowList);
-            for (int col = 0; col < pixelTable.getWidth(); col++) {
-                int xGradientSquare = R_x.apply(row, col)^2 + G_x.apply(row, col)^2 + B_x.apply(row, col)^2;
-                int yGradientSquare = R_y.apply(row, col)^2 + G_y.apply(row, col)^2 + B_y.apply(row, col)^2;
-                rowList.add(xGradientSquare + yGradientSquare);
-            }
-        }
+        mapIndexed((x, y) ->
+                R_x.apply(x, y) ^ 2 + G_x.apply(x, y) ^ 2 + B_x.apply(x, y) ^ 2 +
+                R_y.apply(x, y) ^ 2 + G_y.apply(x, y) ^ 2 + B_y.apply(x, y) ^ 2);
+        dualGradientComputed = true;
     }
 
+    /**
+     * Gives the difference function that computes the central difference of the given color in the given direction
+     * using the pixel location (x, y) given as arguments.
+     *
+     * @param c Color enum
+     * @param d Direction enum
+     * @return Difference function
+     */
     private BiFunction<Integer, Integer, Integer> diffFunc(Color c, Direction d) {
         return d == Direction.X ?
                 (x, y) -> pixelTable.get(x - 1, y).getColor(c) - pixelTable.get(x + 1, y).getColor(c) :
                 (x, y) -> pixelTable.get(x, y - 1).getColor(c) - pixelTable.get(x, y + 1).getColor(c);
     }
 
-    public ArrayList<ArrayList<Integer>> getDualGradientEnergies() {
-        if (dualGradientEnergies == null) computeDualGradientEnergyTable();
-        return dualGradientEnergies;
+    public Integer[][] getDualGradientEnergies() {
+        if (!dualGradientComputed) computeDualGradientEnergyTable();
+        return table;
     }
 }
